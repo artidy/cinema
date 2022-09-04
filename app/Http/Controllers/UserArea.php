92ad725daedbf726c3465bb01d5720ee4db76c4a
Andrey Pechennikov <ap@devandy.me>
2022-09-04 11:08:42 +0300

    Добавил функции для контроллера пользователей

--- app/Http/Controllers/UserArea.php
+++ app/Http/Controllers/UserArea.php
@@ -6,5 +6,43 @@ use Illuminate\Http\Request;
 
 class UserArea extends Controller
 {
-    //
+    public function updateUser(): string
+    {
+        return 'update user';
+    }
+
+    public function getUserFilms(): string
+    {
+        return 'get user films';
+    }
+
+    public function getUserNewEpisodes(): string
+    {
+        return 'get user new episodes';
+    }
+
+    public function addFilmToWatchingList(): string
+    {
+        return 'film was added to watching list';
+    }
+
+    public function removeFilmToWatchingList(): string
+    {
+        return 'film was removed from watching list';
+    }
+
+    public function addEpisodeToWatchingList(): string
+    {
+        return 'episode was added to watching list';
+    }
+
+    public function removeEpisodeToWatchingList(): string
+    {
+        return 'episode was removed from watching list';
+    }
+
+    public function rateFilm(): string
+    {
+        return 'film was rated';
+    }
 }
